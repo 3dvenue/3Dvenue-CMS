417d31dc469333370f9deb37670da2c4d15a9d01
Yoshihiro Murai (村井 芳裕) <rentalspaceluce@gmail.com>
2026-07-26 20:42:53 +0900

    Reomove 3dvenue directory

--- 3dvenue/inc/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* 3Dvenue-CMS Copyright (c) 2026 yoshihiro Murai Licensed under MIT (https://opensource.org/licenses/MIT)*/
-if (basename(__FILE__) === basename($_SERVER['SCRIPT_FILENAME'])) exit;
-?>
-<div class="inner">
-	<div id="logo"><a href="./"><img src="../common/img/logo.webp" alt="logo"></a></div>
-	<h1>3Dvenue</h1>
-</div>
